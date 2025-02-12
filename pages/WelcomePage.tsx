f38394b8ce84cfca0e6592a2aaf0ef165ef75486
vannlithikim <kimvannlithi217@gmail.com>
2025-02-12 16:21:15 +0700

    adding font-next

--- pages/WelcomePage.tsx
+++ pages/WelcomePage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const WelcomePage = () => {
   return (
-    <div className="">
+    <div className="pl-4 pt-4">
        <h1 className="text-[#F9EF19] font-bold text-2xl">Chat BOT </h1>
     </div>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { Inter } from "next/font/google";
+
+// Load the Inter font with Next.js built-in font optimization
+const inter = Inter({ subsets: ["latin"] });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <Component {...pageProps} />
+      <div className={inter.className}> {/* Apply the Inter font globally */}
+        <Component {...pageProps} />
+      </div>
     </ThemeProvider>
   );
 }
