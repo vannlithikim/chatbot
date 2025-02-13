c9983640e48ea2971287fe8862b1fb491d62b5bb
vannlithikim <kimvannlithi217@gmail.com>
2025-02-13 16:30:27 +0700

    add feature description

--- components/InputText.tsx
+++ components/InputText.tsx
@@ -17,7 +17,8 @@ const InputText = () => {
 
       const lineHeight = parseInt(getComputedStyle(textarea).lineHeight, 10);
       const maxHeight = lineHeight * 7;
-      textarea.style.overflowY = textarea.scrollHeight > maxHeight ? "auto" : "hidden";
+      textarea.style.overflowY =
+        textarea.scrollHeight > maxHeight ? "auto" : "hidden";
     }
   }, [message]);
 
@@ -25,8 +26,9 @@ const InputText = () => {
     <div className="relative flex items-center w-full">
       <textarea
         ref={textareaRef}
-        className="flex-1 resize-none text-base px-4 py-3 pr-28 rounded-[14px] border border-[#F9EF19] 
-        bg-[rgba(217,217,217,0.4)] focus:bg-[rgba(217,217,217,0.2)] placeholder-white focus:placeholder-opacity-50"
+        className="flex-1 resize-none text-base px-4 py-3 pr-28 rounded-[14px] border-2 border-gray-500 
+            bg-[rgba(217,217,217,0.4)] focus:bg-[rgba(217,217,217,0.2)] placeholder-white 
+            focus:placeholder-opacity-50 focus:border-[#F9EF19] focus:outline-none"
         placeholder="Type your message..."
         rows={1}
         value={message}
@@ -39,17 +41,17 @@ const InputText = () => {
         flex items-center space-x-2 pr-3`}
       >
         {/* Paperclip Button */}
-        <button className="p-2 rounded-full bg-white text-gray-500 hover:bg-yellow-400 transition">
-          <Paperclip size={20} />
+        <button className="p-2 rounded-full bg-white text-gray-700 hover:bg-yellow-400 transition">
+          <Paperclip size={15} />
         </button>
 
         {/* Send Button */}
         <button
           className={`p-2 rounded-full ${
-            message ? "bg-[#F9E919]" : "bg-gray-300"
-          } text-gray-500 hover:bg-yellow-400 transition`}
+            message ? "bg-[#F9E919]" : "bg-white"
+          } text-gray-700 hover:bg-yellow-400 transition`}
         >
-          <Send size={20} />
+          <Send size={15} />
         </button>
       </div>
     </div>
--- pages/WelcomePage.tsx
+++ pages/WelcomePage.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Image from "next/image";
 import InputText from "@/components/InputText";
+import { featureData } from "@/constants/data";
+import { FeatureInfo } from "@/types/index";
 
 const WelcomePage = () => {
   return (
@@ -9,16 +11,48 @@ const WelcomePage = () => {
       <h1 className="text-[#F9EF19] font-bold text-2xl pl-4 pt-4">Chat BOT</h1>
 
       {/* Logo centered horizontally but positioned below the heading */}
-      <div className="flex flex-col justify-center items-center mt-28">
-        <Image src="/Logo.png" alt="Logo" width={110.94} height={92} />
-        <p className="text-center pt-4 font-bold text-[48px]">
+      <div className="flex flex-col justify-center items-center mt-20">
+        <Image src="/Logo.png" alt="Logo" width={100.94} height={82} />
+        <p className="text-center pt-4 font-bold text-[38px]">
           Welcome to <span className="text-[#F9EF19] ">Chat BOT</span>
         </p>
-        <p className="text-center font-[20px]">&quot;Smart, fast, and always ready to help&quot;</p>
+        <p className="text-center font-[10px]">
+          &quot;Smart, fast, and always ready to help&quot;
+        </p>
       </div>
 
-      <div className="py-11 mx-96">
+      {/* Input field to type and send messages */}
+      <div className="py-10 mx-96">
         <InputText />
+      </div>
+
+      {/* Info Feature */}
+      <div className="flex flex-row justify-center gap-14 text-center py-8">
+        {featureData.map((item: FeatureInfo, index: number) => (
+          <div key={index} className="flex flex-col items-center max-w-[300px] gap-2">
+            <item.icon size={25} className="" />
+            <p className="text-[16px] font-bold truncate">{item.title}</p>
+            <p className="text-[14px] line-clamp-3">{item.description}</p>
+          </div>
+        ))}
+      </div>
+
+      {/* accept the term policy */}
+      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-xs text-gray-400 text-center">
+        <p>
+          By messaging ChatBOT, you agree to our 
+          <a
+            href="#"
+            className="underline text-[#F9EF19] hover:text-[#00BFFF] "
+          >
+            Terms 
+          </a>
+          and have read our 
+          <a href="#" className="underline text-[#F9EF19] hover:text-[#00BFFF]">
+            Privacy Policy
+          </a>
+          .
+        </p>
       </div>
     </section>
   );
